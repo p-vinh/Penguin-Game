6c700ed1cfdf2942f69006035591b266dd3abacb
random-debug <85326677+random-debug@users.noreply.github.com>
2022-10-01 02:03:38 -0700

    Update Button.cs

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Do something with this
 public class Button : MonoBehaviour, Interactable {
     SpriteRenderer spriteRenderer;
     public Animator animator;
